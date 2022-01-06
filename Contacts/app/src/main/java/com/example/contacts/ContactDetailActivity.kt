3632c6c605fdc7dbfde576c48949d89f112f25c0
PEREIRA Deborah <Deborah.Pereira@bordeaux-inp.fr>
2022-01-06 20:10:55 +0100

    Réduction de l'image

--- Contacts/app/src/main/java/com/example/contacts/ContactDetailActivity.kt
+++ Contacts/app/src/main/java/com/example/contacts/ContactDetailActivity.kt
@@ -1,9 +1,11 @@
 package com.example.contacts
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import android.widget.TextView
+import android.provider.ContactsContract
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.launch
